63cbf4485984db113bfe6ecfb40afda0eff65af2
JustHinDoua51 <paulfougeray05@gmail.com>
2023-12-07 23:19:36 +0100

    tiens michmich cadeau

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <header>
+            <div id="links">
+              <NuxtLink to="/options"><IconsOptions/></NuxtLink>
+              <NuxtLink to="/informations"><IconsInformations /></NuxtLink>
+            </div>
+            <NuxtLink to="/">EcoClicker</NuxtLink>
+            <NuxtLink to="/documentation">Documentation</NuxtLink>
+    </header>
     <NuxtPage />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -26,34 +26,43 @@
             toggleButton.textContent = 'Afficher les questions';
         }
     };
+
+    function deplacerDiv() {
+        const cliche1 = document.getElementById('cliche1');
+        // Générer une direction aléatoire (angle en radians)
+        const angle = Math.random() * Math.PI * 2;
+        // Définir la vélocité (vitesse)
+        const velocity = 250;
+        // Calculer les composantes de la vélocité en x et y
+        const velocityX = Math.cos(angle) * velocity;
+        const velocityY = Math.sin(angle) * velocity;
+        // Fonction pour mettre à jour la position de la div à intervalles réguliers
+        function updatePosition() {
+            const rect = cliche1.getBoundingClientRect();
+            const windowWidth = window.innerWidth;
+            const windowHeight = window.innerHeight;
+            // Déplacer la div en utilisant une transformation translate
+            cliche1.style.transform = `translate(${velocityX}px, ${velocityY}px)`;
+        }
+        const intervalId = setInterval(updatePosition, 1000 / 60);
+        setTimeout(() => {
+            clearInterval(intervalId);
+        }, 300);
+    }
 </script>
 
 <template>
     <div>
-        <header>
-            <div>
-                <a href="options">
-                    <IconsOptions/>
-                </a>
-                <a href="informations">
-                    <IconsInformations />
-                </a>
-            </div>
-            <p>EcoClicker</p>
-            <a href="documentation">
-                Documentation
-            </a>
-        </header>
+        
         <div class="game">
             <div class="left">
                 <div id="money">
                     <p id="PointsCount">0 Ecopoints</p>
                     <p id="PointsPerSecond">0 pts/s</p>
                 </div>
-                <div class="planet">
-                    <button id="planet"></button>
-                </div>
+                <div class="planet"></div>
                 <button id="toggleButton" @click="toggleQuestions()">Afficher/Cacher les questions</button>
+                <button id="toggleButton" @click="deplacerDiv()">Go</button>
             </div>
             <div class="right">
                 <div class="tabs">
@@ -63,16 +72,21 @@
                 <div class="tab1 tabContent active">
                     <div class="choices">
                         <ul id="countChoice">
-                            <li>MAX</li>
-                            <li>*1</li>
-                            <li>*10</li>
+                            <li><button>MAX</button></li>
+                            <li><button>x1</button></li>
+                            <li><button>x10</button></li>
+                            <li><button>x100</button></li>
+                            <li><button>x1000</button></li>
                         </ul>
                     </div>
                     <div class="upgrade">
                         <ul id="upgrade">
-                            <li>Arbres</li>
-                            <li>MArche à pied</li>
-                            <li>Velo</li>
+                            <li><button class="upgrade-button"><div class="upgrade-image"></div>Arbre<div class="price">1$</div></button></li>
+                            <li><button class="upgrade-button"><div class="upgrade-image"></div>Arbre<div class="price">10$</div></button></li>
+                            <li><button class="upgrade-button"><div class="upgrade-image"></div>Arbre<div class="price">100$</div></button></li>
+                            <li><button class="upgrade-button"><div class="upgrade-image"></div>Arbre<div class="price">1000$</div></button></li>
+                            <li><button class="upgrade-button"><div class="upgrade-image"></div>Arbre<div class="price">10000$</div></button></li>
+                            <li><button class="upgrade-button"><div class="upgrade-image"></div>Arbre<div class="price">100000$</div></button></li>
                         </ul>
                     </div>
                 </div>
@@ -102,9 +116,14 @@
                     <li><div id="answer4">reponse 4</div></li>
                 </ul>
             </div>
+            
         </div>
+        <div class="cliche" id="cliche1"></div>
+        <div class="cliche" id="cliche2"></div>
+        <div class="cliche" id="cliche3"></div>
+        <div class="cliche" id="cliche4"></div>
+        <div class="cliche" id="cliche5"></div>
+        <div class="cliche" id="cliche6"></div>
     </div>
-
-    
 </template>
 
