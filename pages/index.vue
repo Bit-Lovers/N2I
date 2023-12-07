321b3e9d93af5fa47eb22abd29feabfd945392a1
Michel Vespier <153131874+mvespier@users.noreply.github.com>
2023-12-07 19:14:39 +0100

    Add button to click

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
+<script setup lang="ts">
+const counter = useState('count', () => 0)
+</script>
+
 <template>
     <div>
-        <h1>
-            Ceci est l'index
-        </h1>
+        <div label="Button" @click="counter++">
+            <h1>CLICK</h1>
+        </div>
+        <p>{{ counter }}</p>
     </div>
 </template>
