fe677926163d3cb64d2dca4d11c96a09418afe74
JustHinDoua51 <paulfougeray05@gmail.com>
2023-12-07 18:36:47 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,37 @@
 <template>
     <div>
-        <h1>
-            Ceci est l'index
-        </h1>
+        <header>
+            <div>
+                <a href="options">
+                    <IconsOptions/>
+                </a>
+                <a href="informations">
+                    <IconsInformations />
+                </a>
+            </div>
+            <p>EcoClicker</p>
+            <a href="documentation">
+                Documentation
+            </a>
+        </header>
+        <div class="game">
+            <div class="left">
+                <div class="money">
+
+                </div>
+                <div class="planet">
+
+                </div>
+            </div>
+            <div class="right">
+                <div class="choice">
+
+                </div>
+                <div class="upgrades">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
 </template>
