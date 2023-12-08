4275fb17a745a2c0957819eea9343a283d9da6de
Michel Vespier <153131874+mvespier@users.noreply.github.com>
2023-12-08 05:32:50 +0100

    Ok

--- components/Upgrade.vue
+++ components/Upgrade.vue
@@ -24,12 +24,12 @@ function checkadd(cookieamount:Object, station:Object) {
 if (typeof window !== 'undefined') {
     var interval =  setInterval(() => {
     periodicallyAddValue(cookies,ptsPerSec)
+    periodicallyAddValue(totalCookies,ptsPerSec)
 }, 1000)
 }
 
 function periodicallyAddValue(object: Object, value: number) {
     addValue(object, value);
-    console.log('Amount:', object.amount);
 }
 
 function updatePtsPerSec(amount1 : number, amount2 : number, amount3 : number, amount4 : number, amount5 : number) {
@@ -37,10 +37,11 @@ function updatePtsPerSec(amount1 : number, amount2 : number, amount3 : number, a
     return (amount1 + 2*amount2 + 10*amount3 + 50*amount4 + 100*amount5);
 }
 
-function buyUpgrade(interval : Interval, object : Object, value: number){
+function buyUpgrade(interval : Interval, object, object2 : Object, value: number){
     clearInterval(interval)
     var interval =  setInterval(() => {
         periodicallyAddValue(object,value)
+        periodicallyAddValue(object2,value)
     }, 1000)
     return interval
 }
@@ -50,19 +51,19 @@ function buyUpgrade(interval : Interval, object : Object, value: number){
 <template>
     <ClientOnly>
     <div>
-        <div class="Button noselect" @click="if (cookies.amount >= station1.price) {addValue(station1, 1); addValue(cookies, -station1.price); ptsPerSec = updatePtsPerSec(station1.amount, station2.amount, station3.amount, station4.amount, station5.amount); interval = buyUpgrade(interval, cookies, ptsPerSec)}">
+        <div class="Button noselect" @click="if (cookies.amount >= station1.price) {addValue(station1, 1); addValue(cookies, -station1.price); ptsPerSec = updatePtsPerSec(station1.amount, station2.amount, station3.amount, station4.amount, station5.amount); interval = buyUpgrade(interval, cookies, totalCookies, ptsPerSec)}">
             <h1>Arbres : {{ station1.amount }}</h1>
         </div> 
-        <div class="Button noselect" @click="if (cookies.amount >= station2.price) {addValue(station2, 1); addValue(cookies, -station2.price); ptsPerSec = updatePtsPerSec(station1.amount, station2.amount, station3.amount, station4.amount, station5.amount); interval = buyUpgrade(interval, cookies, ptsPerSec)}">
+        <div class="Button noselect" @click="if (cookies.amount >= station2.price) {addValue(station2, 1); addValue(cookies, -station2.price); ptsPerSec = updatePtsPerSec(station1.amount, station2.amount, station3.amount, station4.amount, station5.amount); interval = buyUpgrade(interval, cookies, totalCookies, ptsPerSec)}">
             <h1>Vélo : {{ station2.amount }}</h1>
         </div>
-        <div class="Button noselect" @click="if (cookies.amount >= station3.price) {addValue(station3, 1); addValue(cookies, -station3.price); ptsPerSec = updatePtsPerSec(station1.amount, station2.amount, station3.amount, station4.amount, station5.amount); interval = buyUpgrade(interval, cookies, ptsPerSec)}">
+        <div class="Button noselect" @click="if (cookies.amount >= station3.price) {addValue(station3, 1); addValue(cookies, -station3.price); ptsPerSec = updatePtsPerSec(station1.amount, station2.amount, station3.amount, station4.amount, station5.amount); interval = buyUpgrade(interval, cookies, totalCookies, ptsPerSec)}">
             <h1>éolienne : {{ station3.amount }}</h1>
         </div>
-        <div class="Button noselect" @click="if (cookies.amount >= station4.price) {addValue(station4, 1); addValue(cookies, -station4.price); ptsPerSec = updatePtsPerSec(station1.amount, station2.amount, station3.amount, station4.amount, station5.amount); interval = buyUpgrade(interval, cookies, ptsPerSec)}">
+        <div class="Button noselect" @click="if (cookies.amount >= station4.price) {addValue(station4, 1); addValue(cookies, -station4.price); ptsPerSec = updatePtsPerSec(station1.amount, station2.amount, station3.amount, station4.amount, station5.amount); interval = buyUpgrade(interval, cookies, totalCookies, ptsPerSec)}">
             <h1>Panneaux solaires : {{ station4.amount }}</h1>
         </div>
-        <div class="Button noselect" @click="if (cookies.amount >= station5.price) {addValue(station5, 1); addValue(cookies, -station5.price); ptsPerSec = updatePtsPerSec(station1.amount, station2.amount, station3.amount, station4.amount, station5.amount); interval = buyUpgrade(interval, cookies, ptsPerSec)}">
+        <div class="Button noselect" @click="if (cookies.amount >= station5.price) {addValue(station5, 1); addValue(cookies, -station5.price); ptsPerSec = updatePtsPerSec(station1.amount, station2.amount, station3.amount, station4.amount, station5.amount); interval = buyUpgrade(interval, cookies, totalCookies, ptsPerSec)}">
             <h1>Zone protégée : {{ station5.amount }}</h1>
         </div>
     </div>
