908a28014ded52f570f62d4495572e9d5a53e4d6
Michel Vespier <153131874+mvespier@users.noreply.github.com>
2023-12-08 04:25:17 +0100

    Enfin !

--- app.vue
+++ app.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
     <header>
-            <div id="links">
-              <NuxtLink to="/options"><IconsOptions/></NuxtLink>
-              <NuxtLink to="/informations"><IconsInformations /></NuxtLink>
-            </div>
-            <NuxtLink to="/">EcoClicker</NuxtLink>
-            <NuxtLink to="/documentation">Documentation</NuxtLink>
+      <div id="links">
+        <NuxtLink to="/options"><IconsOptions/></NuxtLink>
+        <NuxtLink to="/informations"><IconsInformations /></NuxtLink>
+      </div>
+      <NuxtLink to="/">EcoClicker</NuxtLink>
+      <NuxtLink to="/documentation">Documentation</NuxtLink>
     </header>
     <NuxtPage />
   </div>
 </template>
-<script>
-
-
-
-</script>
